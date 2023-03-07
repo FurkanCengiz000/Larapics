c3ec1a0b0762d4f981b123a769363740813bb4c3
Furkan Cengiz <FurkanCengiz000@gmail.com>
2023-03-07 22:21:11 +0300

    Working with Eloquent Events

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -45,4 +45,37 @@ class Image extends Model
         return route("images.{$method}", $this->$key);
     }
 
+    public function getSlug()
+    {
+        $slug = str($this->title)->slug();
+        $numSlugFound = static::where('slug', 'regexp', "^" . $slug . "(-[0-9])?")->count();
+
+        if ($numSlugFound > 0)
+        {
+            return $slug . '-' . $numSlugFound + 1;
+        }
+
+        return $slug;
+
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($image) {
+            if ($image->title)
+            {
+                $image->slug = $image->getSlug();
+                $image->is_published = true;
+            }
+        });
+
+        static::updating(function ($image) {
+            if ($image->title && !$image->slug)
+            {
+                $image->slug = $image->getSlug();
+                $image->is_published = true;
+            }
+        });
+    }
+
 }
