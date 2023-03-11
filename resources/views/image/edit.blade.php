206581c839c262d1505979d3979e91243836b0b0
Furkan Cengiz <FurkanCengiz000@gmail.com>
2023-03-11 18:49:31 +0300

    Refactoring the Edit Image Form

--- resources/views/image/edit.blade.php
+++ resources/views/image/edit.blade.php
@@ -1,20 +1,34 @@
-<x-layout title="Edit New Image">
-    <h1>Edit New Image</h1>
-    
-    <x-form action="{{ $image->route('update') }}" method="PUT">
-    
-        <div>
-            <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" width="400">
+<x-layout title="Update Image">
+    <div class="container-fluid mt-4">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-header">Update Image</div>
+
+                    <div class="card-body">
+                        <x-form action="{{ $image->route('update') }}" method="PUT">
+
+                            <div class="mb-4">
+                                <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" class="img-fluid">
+                            </div>
+
+                            <div class="mb-4">
+                                <label class="form-label" for="title">Title</label>
+                                <input class="form-control @error('title') is-invalid @enderror" type="text" value="{{ old('title', $image->title) }}"
+                                    name="title" id="title" value="{{ old('title') }}">
+                                @error('title')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary">Update</button>
+                                <a href="{{ route('images.index') }}" class="btn btn-outline-secondary">Cancel</a>
+                            </div>
+                        </x-form>
+                    </div>
+                </div>
+            </div>
         </div>
-    
-        <div>
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ old('title', $image->title) }}">
-            @error('title')
-                {{ $message }}
-            @enderror
-        </div>
-    
-        <button type="submit">Update</button>
-    </x-form>
+    </div>
 </x-layout>
