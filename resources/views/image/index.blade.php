7c030b08cf5ba715591a5b61b038a29cb64c4e08
Furkan Cengiz <FurkanCengiz000@gmail.com>
2023-03-14 09:47:24 +0300

    Other authorization stuff

--- resources/views/image/index.blade.php
+++ resources/views/image/index.blade.php
@@ -1,4 +1,23 @@
 <x-layout title="Discover Free Images">
+    <section class="py-3 border-bottom bg-white">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <a href="{{ route('images.create') }}" class="btn btn-primary">
+                        <x-icon src="upload.svg" alt="Upload" class="me-2" />
+                        <span>Upload</span>
+                    </a>
+                </div>
+                <div class="col"></div>
+                <div class="col text-right">
+                    <form class="search-form">
+                        <input type="search" name="q" placeholder="Search..." aria-label="Search..."
+                            autocomplete="off">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
     <div class="container-fluid mt-4">
         @if ($message = session('message'))
             <x-alert type="success" dismissible>
@@ -13,17 +32,21 @@
                         <a href="{{ $image->permalink() }}">
                             <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" class="card-img-top">
                         </a>
-                        @can('update', $image)
+                        @canany(['update', 'delete'], $image)
                             <div class="photo-buttons">
                                 <div>
-                                    <a class="btn btn-sm btn-info me-2" href="{{ $image->route('edit') }}">Edit</a> |
-                                    <x-form action="{{ $image->route('destroy') }}" method="DELETE">
-                                        <button class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')"
-                                            type="submit">Delete</button>
-                                    </x-form>
+                                    @can('update', $image)
+                                        <a class="btn btn-sm btn-info me-2" href="{{ $image->route('edit') }}">Edit</a> |
+                                    @endcan
+                                    @can('delete', $image)
+                                        <x-form action="{{ $image->route('destroy') }}" method="DELETE">
+                                            <button class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')"
+                                                type="submit">Delete</button>
+                                        </x-form>
+                                    @endcan
                                 </div>
                             </div>
-                        @endcan
+                        @endcanany
                     </div>
                 </div>
             @endforeach
