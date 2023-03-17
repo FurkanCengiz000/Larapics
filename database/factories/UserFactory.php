31d3716c87da170d959f627e158946e223dfc752
Furkan Cengiz <FurkanCengiz000@gmail.com>
2023-03-17 10:19:15 +0300

    Build User Setting feature - Modify the user profile schema

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Faker\Provider\en_US\Address;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,9 +18,14 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        fake()->addProvider(new Address(fake()));
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => $email = fake()->unique()->safeEmail(),
+            'username' => strstr($email, '@', true) . rand(100, 120),
+            'city' => rand(0, 1) == 0 ? null : fake()->city(),
+            'country' => rand(0, 1) == 0 ? null : fake()->country(),
+            'about_me' => rand(0, 1) == 0 ? null : fake()->text(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
